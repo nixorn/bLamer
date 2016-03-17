0017611dd3508796701406b09807cc3debfb56ae
Artem Kanev <artem.k@ezscratch.com>
2016-03-17 20:53:27 +0300

    test

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,13 @@ module Lib
     ( someFunc
     ) where
 
+import Control.Applicative
+import Data.Attoparsec.ByteString as P
+import Data.Attoparsec.ByteString.Char8 (char8, endOfLine, isDigit_w8)
+import Data.ByteString (ByteString)
+import Data.Word (Word8)
+import Data.Attoparsec.ByteString.Char8 (isEndOfLine, isHorizontalSpace)
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
